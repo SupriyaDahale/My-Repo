cb0f133453e99ba68ad6d74e08d57f720196fc45
Amruta090 <amrutanikam090@gmail.com>
2024-10-17 18:24:03 +0530

    [Amruta]

--- src/test/java/coverFoxTest/TC1234_CoverFox_ValidateBannerCount.java
+++ src/test/java/coverFoxTest/TC1234_CoverFox_ValidateBannerCount.java
@@ -74,6 +74,7 @@ public class TC1234_CoverFox_ValidateBannerCount extends Base {
 		Assert.assertEquals(textCount, bannerCount,"text count not matching with banner count, TC failed");
 		Reporter.log("Code Changed By Amruta",true);
 		Reporter.log("Code changed By Yogendra", true);
+		Reporter.log("Code changed By Yogendra1", true);
 }
 	@AfterClass
 	public void closeApplication() {
