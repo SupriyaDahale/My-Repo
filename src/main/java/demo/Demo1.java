afff0f51bfee4037538d1bbae559d02b520b45ef
s <s@DESKTOP-RFBKGK2>
2024-10-18 10:23:29 +0530

    [Amruta] 18 oct

--- src/main/java/demo/Demo1.java
+++ src/main/java/demo/Demo1.java
@@ -6,7 +6,7 @@ public class Demo1
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome!");
-		System.out.println("Amruta Nikam");
+		System.out.println("Hi,this is Amruta Nikam");
 		
 	}
 }
